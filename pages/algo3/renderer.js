f000ab56fb267efe73da7401d3860b849a2b575b
Antoine Delétoille <antoinedeletoille@gmail.com>
2021-12-24 18:51:18 +0100

    Début dev algo 3

--- pages/algo3/renderer.js
+++ pages/algo3/renderer.js
@@ -10,6 +10,10 @@ Date      : 24/12/2021
 let inProgress = false
 
 let g_Cercles = []
+let maxX = 0;
+let maxY = 0;
+let minX = 0;
+let minY = 0;
 
 // _G Elements //
 let main = document.querySelector("main");
@@ -25,15 +29,28 @@ let gridHeight = document.getElementById("height-value");
 
 let outputBox = document.getElementById("output-box");
 let outputRectanglesBox = document.getElementById("output-boxes");
-let outputRectangles = document.getElementById("outputRectangles");
+let outputCercle = document.getElementById("outputRectangles");
 let coordinateBox = document.getElementById('box-coordinates');
+
+const INFINITY = 10 ** 10
 
 // Classes //
 class Cercle {
     constructor(x, y, r, id, origin) {
         this.x = x;
         this.y = y;
-        this.r =r;
+        this.r = r;
+        this.id = id;
+        this.origin = origin;
+    }
+}
+
+class Hole {
+    constructor(x1, y1, x2, y2, id, origin) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
         this.id = id;
         this.origin = origin;
     }
@@ -47,9 +64,10 @@ function getRandomIntInclusive(min, max) {
 
 function randomizeCoords() {
     let coords = ""
-    let nbCercle = getRandomIntInclusive(10,100)
+    // let nbCercle = getRandomIntInclusive(10,100)
+    let nbCercle = 5
     for (let i = 0; i < nbCercle; i++) {
-        coords = `${coords + Random(50)}`
+        coords = `${coords + Random(100)}`
         if (i !== nbCercle - 1) coords = `${coords}\n`
     }
     coordinateBox.value = coords
@@ -66,44 +84,107 @@ function Main() {
         if (tailleCanvas === 0) {
             inProgress = false
 
-            return setTimeout(() => { if (!inProgress) { Reset() } }, 2000)
+            return setTimeout(() => {
+                if (!inProgress) {
+                    Reset()
+                }
+            }, 2000)
         }
-        //
-
         // Check Format + Format Box Coords //
         let boxes = processCoords(coordinateBox.value)
-        if (!boxes.success) {
-            state.attributes.style.value = "color:rgb(255, 0, 0)"
-            state.innerHTML = "Failed: Wrong Format"
-            inProgress = false
-
-            return setTimeout(() => { if (!inProgress) { Reset() } }, 2000)
-        }
-        // -------------------------------- //
-
-
-        // Current Sort/Session Info //
-        let sessionInfo = {
-
-
-            StartTime: 0,
-            EndTime: 0,
-            RunTime: 0
-        }
-        // ---------------- //
-
 
         // Set Global Arrays //
-        g_Rectangles = sortByArea(boxes.coords) // Iterate over the biggest rectangles first
+        g_Cercles = boxes.coords
+        g_Holes = [new Hole(0, 0, tailleCanvas, INFINITY, 1, 0)] // First hole the size of the Infinity with origin Zero as break Case Origin
         // ----------------- //
 
+        let test = 0
+        // Start Main Iteration over Rectangles //
+        for (let rid = 0; rid < g_Cercles.length; rid++) {
 
-        g_Rectangles = [] // Reset both global arrays back to empty
-        g_Holes = []      //
+            // Place Current Cercle in Best Hole //
+            const r = g_Cercles[rid].r
 
-        inProgress = false // End of Sort
-        state.attributes.style.value = "color:rgb(0, 255, 0)"
-        state.innerHTML = "Done!"
+            if (rid === 0) {
+                g_Cercles[rid].x = r
+                g_Cercles[rid].y = r
+                maxX += r * 2
+                maxY += r * 2
+            } else {
+                if (tailleCanvas * 2 > maxY + (r * 2)) {
+                    g_Cercles[rid].y = r
+                    g_Cercles[rid].x = maxX + r
+                    test = (Math.sqrt(Math.pow((g_Cercles[rid].x - g_Cercles[rid-1].x),2)+Math.pow((g_Cercles[rid].y - g_Cercles[rid-1].y),2)) - (g_Cercles[rid].r - g_Cercles[rid-1].r))
+                    if (test > 0) {
+                        g_Cercles[rid].x = (maxX + r) - ((maxX + r) - (test/2))
+                    } else {
+                        g_Cercles[rid].x = (maxX + r)
+                    }
+                } else {
+                    g_Cercles[rid].y = maxY + r
+                    g_Cercles[rid].x = maxX + r
+                }
+                if (test > 0) {
+                    // maxX += (r * 2) - (test / 2)
+                    maxX += (r * 2) - (test)
+                } else {
+                    maxX += (r * 2)
+                }
+                maxY += (r * 2)
+            }
+
+
+            // ----------------------------------- //
+
+
+            g_Cercles[rid].origin = 0 // Debugging purposes to test cases that are most redundant, or faulty
+            console.log(g_Cercles)
+        }
+        let endRectArea = 0
+        let shortestHeight = 0
+        g_Cercles.forEach(cercle => {
+            let r = cercle.r
+
+            endRectArea = endRectArea + r * 2
+
+            if ((cercle.r) > shortestHeight) {
+                shortestHeight = cercle.r
+            }
+        })
+        // ------------------------------ //
+
+
+        // (Show + Scale) Height On Canvas //
+        canvas.style.height = shortestHeight * 2 + "px"
+        canvas.height = canvas.clientHeight
+
+        gridHeight.style.marginTop = shortestHeight * 2 + 18 + "px"
+
+        gridHeight.innerHTML = shortestHeight
+        const newHeight = (2 - Math.round(gridHeight.getBoundingClientRect().width) / 2).toString()
+        gridHeight.style.marginLeft = Math.floor(newHeight) + "px"
+        // ------------------------------ //
+
+
+        // (Show + Scale) Width On Canvas //
+        canvas.style.width = tailleCanvas * 2 + "px"
+        canvas.width = canvas.clientWidth
+
+        gridWidth.innerHTML = tailleCanvas
+        const newWidth = (tailleCanvas * 2 - Math.round(gridWidth.getBoundingClientRect().width) / 2).toString()
+        gridWidth.style.marginLeft = Math.floor(newWidth) + "px"
+
+        // ------------------------------ //
+        // Draw + Show Rectangles //
+        g_Cercles.forEach(cercle => {
+            let nCercle = new Cercle(cercle.x, cercle.y, cercle.r, cercle.id, cercle.origin)
+            drawCircle(nCercle)
+        })
+
+        g_Cercles = []
+        g_Holes = []
+
+
     }
 }
 
@@ -121,16 +202,16 @@ function Random(max) {
 
 // Essentials //
 function processCoords(inputCoords) {
-    if (inputCoords === "" || inputCoords.length < 4) return { success: false, message: "c1" }
 
     let coords = []
     let current = new Cercle(0, 0, 0, 0, 0)
+    let switcher = 'r'
 
     for (let i = 0; i < inputCoords.length; i++) {
         const n = inputCoords[i]
 
-       if (n === "\n") {
-            if (!parseInt(current.r)) return { success: false, message: "c3" }
+        if (n === "\n") {
+            if (!parseInt(current.r)) return {success: false, message: "c3"}
 
             current.id = coords.length + 1
 
@@ -139,7 +220,10 @@ function processCoords(inputCoords) {
             current = new Cercle(0, 0, 0, 0, 0)
 
         } else if (n !== " ") {
-            if (!parseInt(n) && n !== "0") return { success: false, message: "c6" }
+            if (!parseInt(n) && n !== "0") return {success: false, message: "c6"}
+
+            if (!current[switcher]) current[switcher] = ""
+            current[switcher] = current[switcher] + n
 
         }
 
@@ -152,9 +236,34 @@ function processCoords(inputCoords) {
         }
     }
 
-    return { success: true, coords: coords }
+    return {success: true, coords: coords}
 }
 
 function clearCanvas() {
     canvasCTX.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function getRandomColor() {
+    color = "hsl(" + Math.random() * 360 + ", 100%, 70%)";
+    return color;
+}
+
+function drawCircle(shape) {
+    const x = shape.x
+    const y = shape.y
+    const r = shape.r
+
+    _drawCircle(x, y, r, getRandomColor(), "rgb(50, 50, 50)") // Draw Rectangle
+
+}
+
+function _drawCircle(x, y, r, fill, stroke) {
+    if ((x != r) || (y != r)) {
+        canvasCTX.moveTo(x,y)
+    }
+    canvasCTX.fillStyle = fill || "rgb(255,255,255)";
+    canvasCTX.strokeStyle = stroke || "rgb(0, 0, 0)"
+    canvasCTX.lineWidth = 1
+    canvasCTX.arc(x, y, r, 0, Math.PI * 2, false);
+    canvasCTX.stroke()
 }
