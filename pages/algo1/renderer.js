1f1f97e7ffb883a61723e321878836cae31c378e
Antoine <antoinedeletoille@gmail.com>
2022-01-25 18:35:21 +0100

    Algorithme de Jackson

--- pages/algo1/renderer.js
+++ pages/algo1/renderer.js
@@ -1,249 +1,312 @@
-
-  class tache {
-    constructor(id,duree, delai) {
-      this.id = id;
+class tache {
+    constructor(id, duree, delai) {
+        this.id = id;
         this.duree = duree;
         this.delai = delai;
     }
-  }
+}
 
-  min = 1;
-  max = 10;
+min = 1;
+max = 10;
 
-  function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
-  var taches = [];
-  var divTache = [];
+var taches = [];
+var divTache = [];
 
-  for (var i = 0; i < 5; i++) {
-    taches.push(new tache(i+1,getRandomIntInclusive(min, max), getRandomIntInclusive(min, max)));
+for (var i = 0; i < 5; i++) {
+    taches.push(new tache(i + 1, getRandomIntInclusive(min, max), getRandomIntInclusive(min, max)));
     divTache.push({
-        tache : document.getElementById(`tache${i+1}`),
-        delai : document.getElementById(`Delaitache${i+1}`)
+        tache: document.getElementById(`tache${i + 1}`),
+        delai: document.getElementById(`Delaitache${i + 1}`)
     });
     divTache[i].tache.innerHTML = taches[i].duree;
     divTache[i].delai.innerHTML = taches[i].delai;
-  }
+}
 
-  var Pi = 0;
-  var Fi = 0;
-  var Di = 0;
-  var R = 0;
+var Pi = 0;
+var Fi = 0;
+var Di = 0;
+var R = 0;
 
-  var total_retard = 0;
-  var somme_retard_initial = 0;
+var total_retard = 0;
+var somme_retard_initial = 0;
 
-  results = [];
+results = [];
 
-  
-  for (var t of taches) {
+
+for (var t of taches) {
     Pi = t.duree;
     Fi += Pi;
     Di = t.delai;
     // R = Fi - Di;
 
     if (R < 0) {
-      R += Fi - Di;
+        R += Fi - Di;
     } else {
-      R = Fi - Di;
+        R = Fi - Di;
     }
 
     results.push(R);
-  }
+}
 
-  console.log(results)
+console.log(results)
 
 
-  for (var c of results) {
+for (var c of results) {
     somme_retard_initial += c
 
-  }
+}
 
-  for (var chiffre of results) {
-    if(chiffre > total_retard){
-      greater_retard_initial = chiffre
+for (var chiffre of results) {
+    if (chiffre > total_retard) {
+        greater_retard_initial = chiffre
     }
-  }
+}
 
-  var moyenne = somme_retard_initial / results.length
+var moyenne = somme_retard_initial / results.length
 
-  var div_somme_retard_initial = document.getElementById("somme_retard_initial");
-  div_somme_retard_initial.innerHTML = somme_retard_initial;
+var div_somme_retard_initial = document.getElementById("somme_retard_initial");
+div_somme_retard_initial.innerHTML = somme_retard_initial;
 
-  var div_greater_retard_initial = document.getElementById("greater_retard_initial");
-  div_greater_retard_initial.innerHTML = greater_retard_initial;
+var div_greater_retard_initial = document.getElementById("greater_retard_initial");
+div_greater_retard_initial.innerHTML = greater_retard_initial;
 
-  var div_moyenne_retard_initial = document.getElementById("moyenne_retard_initial");
-  div_moyenne_retard_initial.innerHTML = moyenne;
+var div_moyenne_retard_initial = document.getElementById("moyenne_retard_initial");
+div_moyenne_retard_initial.innerHTML = moyenne;
 
-  //critère 1 minimiser le plus grand retard
-  function Critere1(){
+//critère 1 minimiser le plus grand retard
+function Critere1() {
     let tachesDuree = taches.sort((a, b) => {
-      return a.duree - b.duree;
+        return a.duree - b.duree;
     });
-  
+
     let tachesSorted = tachesDuree.sort((a, b) => {
-      return a.delai - b.delai;
+        return a.delai - b.delai;
     });
-  
+
     console.log(tachesDuree);
-  
+
     console.log(tachesSorted)
-  
+
     var Pi = 0;
     var Fi = 0;
     var Di = 0;
     var R = 0;
-  
+
     results = [];
-  
-    
+
+
     for (var t of tachesSorted) {
-      Pi = t.duree;
-      Fi += Pi;
-      Di = t.delai;
-      // R = Fi - Di;
-  
-      if (R < 0) {
-        R += Fi - Di;
-      } else {
-        R = Fi - Di;
-      }
-  
-      results.push(R);
+        Pi = t.duree;
+        Fi += Pi;
+        Di = t.delai;
+        // R = Fi - Di;
+
+        if (R < 0) {
+            R += Fi - Di;
+        } else {
+            R = Fi - Di;
+        }
+
+        results.push(R);
     }
-  
-  
-  
+
+
     console.log(results);
-  
+
     var total_retard = 0;
-  
+
     for (var chiffre of results) {
-      if(chiffre > total_retard){
-        total_retard = chiffre
-      }
+        if (chiffre > total_retard) {
+            total_retard = chiffre
+        }
     }
-  
+
     console.log(total_retard);
-  
+
     var divretard = document.getElementById("retard_final");
     divretard.innerHTML = total_retard;
-  }
- 
-
-//critère 2 minimiser le retard moyen
-
-function critere2(){
-  let tachesDuree = taches.sort((a, b) => {
-    return a.duree - b.duree;
-  });
-  
-  let tachesSorted = tachesDuree.sort((a, b) => {
-    return a.delai - b.delai;
-  });
-  
-  console.log(tachesDuree);
-  
-  console.log(tachesSorted)
-  
-  var Pi = 0;
-  var Fi = 0;
-  var Di = 0;
-  var R = 0;
-  
-  results = [];
-  
-  
-  for (var t of tachesSorted) {
-    Pi = t.duree;
-    Fi += Pi;
-    Di = t.delai;
-    // R = Fi - Di;
-  
-    if (R < 0) {
-      R += Fi - Di;
-    } else {
-      R = Fi - Di;
-    }
-  
-    results.push(R);
-  }
-  
-  
-  
-  console.log(results);
-  
-  var total_retard = 0;
-  
-  for (var chiffre of results) {
-      total_retard += chiffre
-  
-  }
-  
-  var moyenne_c2 = total_retard / results.length
-  
-  
-  console.log(moyenne_c2);
-
-  var divretard = document.getElementById("retard_final");
-divretard.innerHTML = moyenne_c2;
 }
 
 
+//critère 2 minimiser le retard moyen
+
+function critere2() {
+    let tachesDuree = taches.sort((a, b) => {
+        return a.duree - b.duree;
+    });
+
+    let tachesSorted = tachesDuree.sort((a, b) => {
+        return a.delai - b.delai;
+    });
+
+    console.log(tachesDuree);
+
+    console.log(tachesSorted)
+
+    var Pi = 0;
+    var Fi = 0;
+    var Di = 0;
+    var R = 0;
+
+    results = [];
 
 
-  //critère 3 minimiser la somme des retards
-function critere3(){
-  let tachesDuree = taches.sort((a, b) => {
-    return a.duree - b.duree;
-  });
+    for (var t of tachesSorted) {
+        Pi = t.duree;
+        Fi += Pi;
+        Di = t.delai;
+        // R = Fi - Di;
 
-console.log(tachesDuree)
+        if (R < 0) {
+            R += Fi - Di;
+        } else {
+            R = Fi - Di;
+        }
 
-
-  var Pi = 0;
-  var Fi = 0;
-  var Di = 0;
-  var R = 0;
-
-  results = [];
-
-  
-  for (var t of tachesDuree) {
-    Pi = t.duree;
-    Fi += Pi;
-    Di = t.delai;
-    // R = Fi - Di;
-
-    if (R < 0) {
-      R += Fi - Di;
-    } else {
-      R = Fi - Di;
+        results.push(R);
     }
 
-    results.push(R);
-  }
+
+    console.log(results);
+
+    var total_retard = 0;
+
+    for (var chiffre of results) {
+        total_retard += chiffre
+
+    }
+
+    var moyenne_c2 = total_retard / results.length
 
 
+    console.log(moyenne_c2);
 
-  console.log(results);
+    var divretard = document.getElementById("retard_final");
+    divretard.innerHTML = moyenne_c2;
+}
 
-  var total_retard = 0;
 
-  for (var chiffre of results) {
-      total_retard += chiffre
+//critère 3 minimiser la somme des retards
+function critere3() {
+    let tachesDuree = taches.sort((a, b) => {
+        return a.duree - b.duree;
+    });
 
-  }
+    console.log(tachesDuree)
 
-  console.log(total_retard);
 
-  var divretard = document.getElementById("retard_final");
-  divretard.innerHTML = total_retard;
+    var Pi = 0;
+    var Fi = 0;
+    var Di = 0;
+    var R = 0;
+
+    results = [];
+
+
+    for (var t of tachesDuree) {
+        Pi = t.duree;
+        Fi += Pi;
+        Di = t.delai;
+        // R = Fi - Di;
+
+        if (R < 0) {
+            R += Fi - Di;
+        } else {
+            R = Fi - Di;
+        }
+
+        results.push(R);
+    }
+
+
+    console.log(results);
+
+    var total_retard = 0;
+
+    for (var chiffre of results) {
+        total_retard += chiffre
+
+    }
+
+    console.log(total_retard);
+
+    var divretard = document.getElementById("retard_final");
+    divretard.innerHTML = total_retard;
+}
+
+
+function algo_Jackson() {
+    // machine 1 m1 et machine 2 m2
+    var m1 = [];
+    var m2 = [];
+    var divMachine1 = []
+    var divMachine2 = []
+
+    for (var i = 0; i < 5; i++) {
+        m1.push(new tache(i + 1, getRandomIntInclusive(1, 200)));
+        divMachine1.push({
+            tache: document.getElementById(`tacheM1${i + 1}`),
+        });
+        divMachine1[i].tache.innerHTML = m1[i].duree;
+    }
+
+    for (var i = 0; i < 5; i++) {
+        m2.push(new tache(i + 1, getRandomIntInclusive(1, 200)));
+        divMachine2.push({
+            tache: document.getElementById(`tacheM2${i + 1}`),
+        });
+        divMachine2[i].tache.innerHTML = m2[i].duree;
+    }
+    var iteration = 1
+    var valTacheMin = 10 ** 10;
+    var machineMin;
+    var rangValeurMin;
+    var minRang = 1
+    var maxRang = 5
+    var tabRangValidé = []
+    var tabRang = [-1, -1, -1, -1, -1]
+
+    while (iteration <= 5) {
+        for (var i = 0; i < 5; i++) {
+            if (tabRangValidé.includes(i) === false) {
+                if (valTacheMin > m1[i].duree) {
+                    valTacheMin = m1[i].duree
+                    machineMin = 1
+                    rangValeurMin = i
+                }
+
+                if (valTacheMin > m2[i].duree) {
+                    valTacheMin = m2[i].duree
+                    machineMin = 2
+                    rangValeurMin = i
+                }
+            }
+        }
+
+        if (machineMin === 1) {
+            tabRang[rangValeurMin] = minRang
+            minRang++
+        } else {
+            tabRang[rangValeurMin] = maxRang
+            maxRang--
+        }
+        tabRangValidé.push(rangValeurMin)
+        valTacheMin = 10 ** 10;
+        iteration++
+    }
+    var divRang = [];
+    for (var i = 0; i < 5; i++) {
+        divRang.push({
+            tache: document.getElementById(`rang${i + 1}`),
+        });
+        divRang[i].tache.innerHTML = tabRang[i];
+    }
 }
 
 
